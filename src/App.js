abe4533083deed2e59772663bb6a353716b9d3b0
cgonz0 <cgonzadesign@gmail.com>
2018-10-21 17:53:06 -0700

    commit project, react-bootstrap & search

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { FormGroup, FormControl, InputGroup, Glyphicon} from 'react-bootstrap';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      query: ''
+    };
+    // this.handleChange = this.handleChange.bind(this);
+    // this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange = event => {
+    this.setState({
+      query: event.target.value
+    });
+  }
+
+  search() {
+    console.log('this.state', this.state)
+  }
+
+
+  handleEnterKeyPress = event => {
+    if (event.key === 'Enter') {
+      this.search();
+    }
+  }
+
+  handleClick = event => {
+    this.search()
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <div className="App-title">BopSnip</div>
+
+       <FormGroup>
+        <InputGroup>
+          <FormControl
+            type="text"
+            placeholder="Search for an Artist"
+            value={this.state.query}
+            onChange={this.handleChange}
+            onKeyPress={this.handleEnterKeyPress}
+          />
+          <InputGroup.Addon onClick={this.handleClick}>
+            <Glyphicon glyph="search"></Glyphicon>
+          </InputGroup.Addon>
+        </InputGroup>
+       </FormGroup>
+
+       <div className="Profile">
+        <div>
+          Artist Photo
+        </div>
+        <div>
+          Artist Name
+        </div>
+       </div>
+
+       <div className="Gallery">
+        <div>Gallery</div>
+       </div>
+
       </div>
     );
   }
