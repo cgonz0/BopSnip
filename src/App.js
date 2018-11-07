88847c07fd0cb4259a5627f5e7c5c356f2ee9479
cgonz0 <cgonzadesign@gmail.com>
2018-11-07 10:47:24 -0800

    hide templating & expand fetch request (albums)

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ class App extends Component {
     super(props);
     this.state = {
       query: '',
-      artist: null
+      artist: null,
+      //initialize tracks to empty array
+      tracks: []
     };
     // this.handleChange = this.handleChange.bind(this);
     // this.handleClick = this.handleClick.bind(this);
@@ -25,8 +27,9 @@ class App extends Component {
   search() {
     console.log('this.state', this.state)
     const BASE_URL = 'https://api.spotify.com/v1/search?';
-    const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
-    const accessToken = '';
+    let FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+    const ALBUM_URL = 'https://api.spotify.com/v1/artists/';
+    const accessToken = 'BQBNEZRy801yFecHAcfR8OKPJSnfP-BCcSUaEaN4utrqGIqyAElwOVUfuw77sSML0s_iABMghLmC_GRVFPjbQKys_7vJjF611ULHLpJHX7DUzrI67uKu51iO02LctT8VCJFqdal8xOxUS5IOtce4syHqH_bvHYVm_Hvj&refresh_token=AQA2RqwKDqC1U8XS_fHocSlAHikkyhIOWiMYzN14d1mlFX7tGFpH7tP2Zx9_MNUoUY0fkVBzat3fiAVPJ-LaUjoiDT91btRDrROJk8krwcm0fXYwXFZ1Vf9qtDKYXnqe__YWjg';
 
     // const myHeaders = new Headers();
 
@@ -45,6 +48,24 @@ class App extends Component {
         const artist = json.artists.items[0];
         console.log('artist', artist);
         this.setState({artist});
+
+        //Fetch albums
+        FETCH_URL = `${ALBUM_URL}${artist.id}/top-tracks?country=US&`
+        fetch(FETCH_URL, myOptions)
+        .then(response => response.json())
+        .then(json => {
+          console.log('artist top tracks:', json);
+          //set tracks to state
+
+          //variable name and key we want to access within function or obj is same (useful for multiple keys:
+          // const tracks = json.tracks;
+          const { tracks } = json;
+
+          this.setState({tracks});
+        })
+
+
+
       });
   }
 
@@ -81,13 +102,19 @@ class App extends Component {
         </InputGroup>
        </FormGroup>
 
-       <Profile
-        artist={this.state.artist}
-       />
+       {
+         this.state.artist !== null
+          ?<div>
+             <Profile
+              artist={this.state.artist}
+             />
+            <div className="Gallery">
+            <div>Gallery</div>
+            </div>
+          </div>
 
-       <div className="Gallery">
-        <div>Gallery</div>
-       </div>
+          : <div></div>
+       }
 
       </div>
     );
