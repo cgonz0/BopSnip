4bfc688d1d7d07d8a94960dce6d8eff68c360d71
cgonz0 <cgonzadesign@gmail.com>
2019-01-23 15:15:17 -0800

    fix authentication

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { FormGroup, FormControl, InputGroup, Glyphicon} from 'react-bootstrap';
 import Profile from './Profile';
 import Gallery from './Gallery';
+import Spotify from './Spotify';
 
 class App extends Component {
   constructor(props){
@@ -26,19 +26,18 @@ class App extends Component {
   }
 
   search() {
-    // console.log('this.state', this.state)
+
+    const accessToken = Spotify.getAccessToken();
+
     const BASE_URL = 'https://api.spotify.com/v1/search?';
     let FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
     const ALBUM_URL = 'https://api.spotify.com/v1/artists/';
-    const accessToken = '';
-
-    // const myHeaders = new Headers();
 
     const myOptions = {
       method: 'GET',
       headers:  {
         Authorization: `Bearer ${accessToken}`
-     },
+      },
       mode: 'cors',
       cache: 'default'
     };
@@ -65,8 +64,7 @@ class App extends Component {
           this.setState({tracks});
         })
       });
-  }
-
+    }
 
   handleEnterKeyPress = event => {
     if (event.key === 'Enter') {
@@ -77,7 +75,6 @@ class App extends Component {
   handleClick = event => {
     this.search()
   }
-
 
   render() {
     return (
---
+++ src/Spotify.js
@@ -0,0 +1,31 @@
+let accessToken;
+let expiresIn;
+// const redirectURI = 'http://bopsnip.surge.sh';
+const redirectURI = 'http://localhost:3000/';
+const clientID = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+
+const Spotify = {
+  getAccessToken() {
+    if (accessToken) {
+      return accessToken;
+    }
+
+    let URLToken = window.location.href.match(/access_token=([^&]*)/);
+    let URLExp = window.location.href.match(/expires_in=([^&]*)/);
+
+    if (URLToken && URLExp) {
+      accessToken = URLToken[1];
+      //return Number not string
+      expiresIn = Number(URLExp[1]);
+      //wipe access tokens and URL params
+      window.setTimeout(() => accessToken = '', expiresIn * 1000);
+      window.history.pushState('Access Token', null, '/');
+      return accessToken;
+    } else {
+      window.location.replace(`https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`);
+    }
+  }
+
+}
+
+export default Spotify;
